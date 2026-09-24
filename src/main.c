d1004af16b7af1091414bd3047e190a82460b9b8
Kentelnas2022 <kentzorelelnas.202200472@gmail.com>
2026-09-25 06:38:26 +0800

    Configure initial Wokwi simulation.

--- src/main.c
+++ src/main.c
@@ -1,1 +1,8 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main() {
+    printf("BCA152 FreeRTOS Multisensor\n");
+    printf("System starting...\n");
+}
